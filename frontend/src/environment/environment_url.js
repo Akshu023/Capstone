463cefbfaf143f5892aee58f414e9cd86fdce4f5
Akshu <144461057+Akshu023@users.noreply.github.com>
2026-03-22 20:16:35 +0530

    Create environment_url.js

---
+++ frontend/src/environment/environment_url.js
@@ -0,0 +1,15 @@
+import { dev } from "./environment_constant";
+
+const baseUrl = dev;
+
+const url = {
+  auth: {
+    register: baseUrl + "register",
+    login: baseUrl + "login",
+  },
+  product: {
+    add_product: baseUrl + "add-product",
+    get_product: baseUrl + "get-products"
+  }
+};
+export { url };
